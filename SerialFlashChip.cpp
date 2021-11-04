1d409016c64524fdee7e4dcab76c077fa013eca1
Sandro Grassia <41198542+SandroGrassia@users.noreply.github.com>
2021-11-04 01:43:17 +0100

    Winbond W25Q512 support
    Hi Paul, since W25Q512 id[2] is 38h (56d) , I simply added this check in order to recognize the chip, and set the capacity to 64MB. With these additional lines my T4.1 correctly see and use the chip.

--- SerialFlashChip.cpp
+++ SerialFlashChip.cpp
@@ -465,7 +465,11 @@ uint32_t SerialFlashChip::capacity(const uint8_t *id)
 	if ((id[0]==0 && id[1]==0 && id[2]==0) || 
 		(id[0]==255 && id[1]==255 && id[2]==255)) {
 		n = 0;
+	} else
+	if (id[2] == 56) {
+		n = 1ul << 26; // 2^26 = 64M
 	}
+	
 	//Serial.printf("capacity %lu\n", n);
 	return n;
 }
